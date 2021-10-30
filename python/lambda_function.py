d7385c02904ac0848254cadd269786993aa0d52f
AndrewDongminYoo <ydm2790@gmail.com>
2021-10-30 22:44:57 +0900

    init

---
+++ python/lambda_function.py
@@ -0,0 +1,78 @@
+import json
+import boto3.session
+import base64
+import pymysql
+from requests_toolbelt.multipart import decoder
+
+
+def create_connection_token():
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name="us-east-1"
+    )
+    get_secret_value_response = client.get_secret_value(
+        SecretId='database-access-key'
+    )
+    token = get_secret_value_response['SecretString']
+    return eval(token)
+
+
+def db_ops():
+    secrets = create_connection_token()
+    try:
+        connection = pymysql.connect(
+            host=secrets['host'],
+            user=secrets['username'],
+            password=secrets['password'],
+            port=secrets['port'],
+            db='sparta',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    except pymysql.MySQLError as e:
+        print("connection error!!")
+        return e
+
+    print("connection ok!!")
+    return connection
+
+
+def uploadToS3(body, original_file_name):
+    s3 = boto3.client('s3', region_name="us-east-1")
+    s3.put_object(
+        ACL="public-read",
+        Bucket='itwassummer.shop',
+        Body=body,
+        Key=original_file_name,
+        ContentType='images/' + original_file_name.split('.')[1]
+    )
+
+    conn = db_ops()
+    cursor = conn.cursor()
+    cursor.execute("insert into image (url) value('" + original_file_name + "')")
+    conn.commit()
+
+
+def lambda_handler(event, context):
+    if 'Content-Type' in event['headers']:
+        content_type_header = event['headers']['Content-Type']
+    else:
+        content_type_header = event['headers']['content-type']
+    print(context)
+    post_data = base64.b64decode(json.loads(event["body"])).decode('iso-8859-1')
+    lst = []
+    for part in decoder.MultipartDecoder(post_data.encode('utf-8'), content_type_header).parts:
+        lst.append(part.text)
+
+    decoder_file = decoder.MultipartDecoder(post_data.encode('utf-8'), content_type_header)
+    print(decoder_file)
+    file_name = lst[1]  # 파일명은 한글이 아니어야 한다.
+    uploadToS3(lst[0].encode('iso-8859-1'), file_name)
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "success",
+        }),
+    }
